77760b012aefc9f715c38dee2c0f8d9578742ee2
Blanca <blancacano2.71828@gmail.com>
2021-06-05 09:46:35 +0200

    [Preparación general] Subo el material inicial para poder empezar a trabajar

---
+++ codigo/main.py
@@ -0,0 +1,109 @@
+####################################################
+#   Trabajo final: Communities And Crimes
+####################################################
+#   Autores:
+#      - Alejandro Borrego Megías
+#      - Blanca Cano Camarero
+#   Fecha: Principios junio 2021
+####################################################
+
+
+#############################
+#######  BIBLIOTECAS  #######
+#############################
+# Biblioteca lectura de datos
+# ==========================
+import pandas as pd
+
+# matemáticas
+# ==========================
+import numpy as np
+
+# Modelos lineales de clasificación a usar   
+# =========================================
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.svm import SVR
+from sklearn.linear_model import SGDRegressor
+
+
+# Preprocesado 
+# ==========================
+from sklearn.model_selection import train_test_split
+
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import scale
+
+# visualización de datos
+# ==========================
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import seaborn as sns # utilizado para pintar la matriz de correlación 
+
+# Validación cruzada
+# ==========================
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
+
+
+# metricas
+# ==========================
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import accuracy_score
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
+
+# Otros
+# ==========================
+from operator import itemgetter #ordenar lista
+import time
+
+np.random.seed(1)
+
+############# Constantes básicas  ############
+
+NOMBRE_FICHERO  = './datos/communities.data'
+SEPARADOR_REGRESION = ','
+
+NUMERO_CPUS_PARALELO = 4 
+
+
+######################## Funciones básicas ##############
+
+def Parada(mensaje = None):
+    '''
+    Hace parada del código y muestra un menaje en tal caso 
+    '''
+    print('\n-------- fin apartado, enter para continuar -------\n')
+    #input('\n-------- fin apartado, enter para continuar -------\n')
+    
+    if mensaje:
+        print('\n' + mensaje)
+
+
+        
+def LeerDatos (nombre_fichero, separador):
+    '''
+    Input: 
+    - file_name: nombre del fichero path relativo a dónde se ejecute o absoluto
+    La estructura de los datos debe ser: 
+       - Cada fila un vector de características con su etiqueta en la última columna.
+
+    Outputs: x,y
+    x: matriz de filas de vector de características
+    y: vector fila de la etiquetas 
+    
+    '''
+    data = np.genfromtxt(nombre_fichero,delimiter=separador)
+    y = data[:,-1].copy()
+    x = data[:,:-1].copy()
+
+    return x,y
+
+
+
---
+++ memoria/como_escribir_cosas.md
@@ -0,0 +1,18 @@
+
+# Ejemplos de cómo poner las imágenes
+\begin{figure}[!h]
+\centering
+\includegraphics[width=1\textwidth]{./imagenes/clasificacion/hinge_0_01.png}
+\caption{Evaluando SGD Classifier con tasa de aprendizaje optima (variable) y factor de regularización 0.01 y función de perdida hinge}
+\end{figure}.   
+
+
+\begin{figure}[h!]
+\begin{subfigure}[b]{0.45\linewidth}
+\includegraphics[width=1\textwidth]{./imagenes/clasificacion/Perceptron_001.png}
+\caption{Perceptrón con tasa aprendizaje = 0.001}
+\end{subfigure}
+\begin{subfigure}[b]{0.45\linewidth}
+\includegraphics[width=1\textwidth]{/home/blanca/repositorios/aprendizaje-automatico/practica3/memoria/imagenes/clasificacion/Perceptron_01.png}
+\caption{Perceptrón con tasa aprendizaje = 0.01}
+\end{subfigure}
